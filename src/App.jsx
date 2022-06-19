d7225e6c29f2eddbd6ba005169135d27e7136ee6
Zhou Yuhang <zyuhang255@gmail.com>
2022-06-19 11:53:09 +0800

    Complete project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Quote from "./Quote";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Quote />
+      </div>
+    );
+  }
+}
---
+++ src/Quote.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+export default class Quote extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quote: "",
+      author: "",
+    };
+  }
+  render() {
+    return (
+      <div id="quote-box">
+        <h4 id="text">"{this.state.quote}"</h4>
+        <h4 id="author">- {this.state.author}</h4>
+        <button id="new-quote" onClick={this.generateQuote}>
+          New Quote
+        </button>
+        <a
+          href={`https://twitter.com/intent/tweet/?text="${this.state.quote}" ${this.state.author}&hashtags=quotes`}
+          target="_blank"
+          id="tweet-button"
+        >
+          <i class="fa-brands fa-twitter-square fa-lg"></i>
+        </a>
+      </div>
+    );
+  }
+
+  componentDidMount = () => {
+    this.generateQuote();
+  };
+  generateQuote = async () => {
+    let response = await fetch("http://api.quotable.io/random", {
+      method: "GET",
+    });
+    let res = await response.json();
+    this.setState({ quote: res.content, author: res.author });
+  };
+}
